1262e72d6555e5447b0f1e3ae8fcb0bc163b8d7d
shreyvijayvargiya <shreyvijayvargiya26@gmail.com>
2024-01-21 01:29:02 +0530

    dynamic modal responsivenss handled

--- components/Projects/CrazyModal.jsx
+++ components/Projects/CrazyModal.jsx
@@ -24,7 +24,7 @@ const DammModal = () => {
 			});
 		} else if (percent >= 5) {
 			gsap.to(ref.current, {
-				width: percent + "%",
+				width: percent >= 50 ? "80%" : percent + "%",
 				scale: 1,
 				opacity: 1,
 			});
@@ -41,7 +41,7 @@ const DammModal = () => {
 					scale: 1,
 				}
 			);
-			gsap.to(modal.current, { opacity: 1, width: "20%" });
+			gsap.to(modal.current, { opacity: 1, width: "100%" });
 			setShow(true);
 		}
 	};
@@ -70,18 +70,21 @@ const DammModal = () => {
 			ref={containerRef}
 		>
 			<div
-				className="fixed top-20 h-4/5 left-60 right-60 mx-auto text-gray-400 hover:text-gray-200 border-dotted border-2 border-gray-600 p-4 rounded-xl cursor-pointer"
+				className="fixed md:top-40 sm:top-20 xs:top-20 xxs:top-20 md:h-4/5 sm:h-3/5 xs:h-3/5 xxs:h-3/5 md:left-60 md:right-60 sm:left-10 xs:left-10 xxs:left-10 sm:right-20 mx-auto text-gray-400 hover:text-gray-200 border-dotted border-2 border-gray-600 p-4 rounded-xl cursor-pointer"
 				style={{ boxShadow: "0px 0px 50px rgb(255, 255, 255, 0.2)" }}
 				ref={ref}
 			>
 				<div className="modal-content bg-none break-words flex-wrap">
-					<p className="paragraph text-7xl">Dynamic Modal</p>
+					<p className="paragraph md:text-7xl">Dynamic Modal</p>
 					<p className="paragraph">Increase width on scroll</p>
 				</div>
 			</div>
-			<div ref={modal} className="fixed top-10 left-10 mx-auto cursor-pointer">
+			<div
+				ref={modal}
+				className="fixed md:top-10 md:left-10 sm:bottom-10 xxs:bottom-10 xs:bottom-10 sm:text-center xxs:text-center xs:text-center mx-auto cursor-pointer"
+			>
 				<p className="text-pink-600 font-mono text-center text-7xl underline">
-					{scrollPercent}%
+					{scrollPercent >= 50 ? 100 : scrollPercent}%
 				</p>
 				<p className="text-gray-600 text-sm text-center">modal width </p>
 			</div>
